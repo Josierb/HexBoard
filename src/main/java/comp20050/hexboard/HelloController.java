08016f9398c50efd2acebad9c54f7f88ee7ae34d
Josie Burke <josierb18@gmail.com>
2025-02-20 17:48:47 +0000

    Initial commit

---
+++ src/main/java/comp20050/hexboard/HelloController.java
@@ -0,0 +1,14 @@
+package comp20050.hexboard;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
