a361e2d2b595ce80133ccf03e5f16febed3f9045
Josie Burke <josierb18@gmail.com>
2025-02-20 19:36:52 +0000

    this is my MESSAGE

--- src/main/java/comp20050/hexboard/HelloApplication.java
+++ src/main/java/comp20050/hexboard/HelloApplication.java
@@ -4,10 +4,30 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("hellow-view.fxml"));
+        primaryStage.setTitle("Hexagon Grid");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+
+
+
+    /*
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -19,5 +39,5 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
-    }
+    }*/
 }
